fbb27caf5a50179b80d4fa09ba954a776b2b2e50
p2006393 <arthur.desbiaux@etu.univ-lyon1.fr>
2024-09-12 16:32:01 +0200

    Trying hyperparamater tuning

--- shallowNet.py
+++ shallowNet.py
@@ -29,49 +29,57 @@ loss_func = torch.nn.MSELoss(reduction='mean')
 train_dataset = torch.utils.data.TensorDataset(data_train,label_train)
 test_dataset = torch.utils.data.TensorDataset(data_test,label_test)
 train_dataset, val_dataset = random_split(train_dataset, [0.8, 0.2])
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
+
 
 models = []
 
-while():
-	new_batch_size = 5 # nombre de données lues à chaque fois
-	new_nb_epochs = 10 # nombre de fois que la base de données sera lue
-	new_hidden_num = 250 # nombre de neurones cachés
-	new_eta = 0.00001 # taux d'apprentissage
-	
-	model = ShallowNet(784, hidden_num, 10)
+for batch_size in range(5, 30, 5):
+	train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+	val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+	test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
+	for hidden_num in range(50, 300, 50):
+		model = ShallowNet(784, hidden_num, 10)
+		for eta in [0.00001, 0.0001, 0.001, 0.01]:
+			optim = torch.optim.SGD(model.parameters(), lr=eta)
+			for nb_epochs in range(10, 30, 10):
+				# print('batch value: \n', batch_size)
+				# print('nb_epoch value: \n', nb_epochs)
+				# print('hidden_run: ', hidden_num)
+				# print('eta:', eta)
+				# on initiliase l'optimiseur
 
-	# on initiliase l'optimiseur
-	optim = torch.optim.SGD(model.parameters(), lr=eta)
+				for n in range(nb_epochs):
+					# on lit toutes les données d'apprentissage
+					for x,t in train_loader:
+						# on calcule la sortie du modèle
+						y = model(x)
+						# on met à jour les poids
+						loss = loss_func(t,y)
+						loss.backward()
+						optim.step()
+						optim.zero_grad()
 
-	valid_losses = []
+					model.eval() # prep model for evaluation
+					for x, t in val_loader:
+						# forward pass: compute predicted outputs by passing inputs to the model
+						y = model(x)
+						# calculate the loss
+						loss = loss_func(t,y)
+						# record validation loss
+						if n == nb_epochs:
+							models.append([model, loss.item(), batch_size, hidden_num, eta, nb_epochs])
+							print('batch value: \n', bestModel[2])
+							print('nb_epoch value: \n', bestModel[3])
+							print('hidden_run: ', bestModel[4])
+							print('eta:', bestModel[5])
 
-	for n in range(nb_epochs):
-		# on lit toutes les données d'apprentissage
-		for x,t in train_loader:
-			# on calcule la sortie du modèle
-			y = model(x)
-			# on met à jour les poids
-			loss = loss_func(t,y)
-			loss.backward()
-			optim.step()
-			optim.zero_grad()
-
-		model.eval() # prep model for evaluation
-		for x, t in val_loader:
-			# forward pass: compute predicted outputs by passing inputs to the model
-			y = model(x)
-			# calculate the loss
-			loss = loss_func(t,y)
-			# record validation loss
-			valid_losses.append(loss.item())
-		
-		models.append(model, np.mean(valid_losses))
-
-		
-# test du modèle (on évalue la progression pendant l'apprentissage)
+bestModel = min(models, key=lambda x: x[1])		
+model = bestModel[0]
+print("Meilleur hyper-paramètre \n")
+print('batch value: \n', bestModel[2])
+print('nb_epoch value: \n', bestModel[3])
+print('hidden_run: ', bestModel[4])
+print('eta:', bestModel[5])
 acc = 0.
 # on lit toutes les donnéees de test
 for x,t in test_loader:
