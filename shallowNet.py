1c7d997297f9f92d4d6202687b464c06efa4ee3d
p2006393 <arthur.desbiaux@etu.univ-lyon1.fr>
2024-09-12 17:19:48 +0200

    Fixing hyperparameter tuning

--- shallowNet.py
+++ shallowNet.py
@@ -33,19 +33,19 @@ train_dataset, val_dataset = random_split(train_dataset, [0.8, 0.2])
 
 models = []
 
-for batch_size in range(5, 30, 5):
+for batch_size in [3, 5, 10]:
 	train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 	val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 	test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
-	for hidden_num in range(50, 300, 50):
+	for hidden_num in [150, 200, 250, 300]:
 		model = ShallowNet(784, hidden_num, 10)
 		for eta in [0.00001, 0.0001, 0.001, 0.01]:
 			optim = torch.optim.SGD(model.parameters(), lr=eta)
-			for nb_epochs in range(10, 30, 10):
-				# print('batch value: \n', batch_size)
-				# print('nb_epoch value: \n', nb_epochs)
-				# print('hidden_run: ', hidden_num)
-				# print('eta:', eta)
+			for nb_epochs in [5, 10, 20]:
+				print('batch value: \n', batch_size)
+				print('nb_epoch value: \n', nb_epochs)
+				print('hidden_run: ', hidden_num)
+				print('eta:', eta)
 				# on initiliase l'optimiseur
 
 				for n in range(nb_epochs):
@@ -58,28 +58,26 @@ for batch_size in range(5, 30, 5):
 						loss.backward()
 						optim.step()
 						optim.zero_grad()
-
+				
+					loss_total = 0
 					model.eval() # prep model for evaluation
 					for x, t in val_loader:
 						# forward pass: compute predicted outputs by passing inputs to the model
 						y = model(x)
 						# calculate the loss
 						loss = loss_func(t,y)
+						loss_total += loss
 						# record validation loss
-						if n == nb_epochs:
-							models.append([model, loss.item(), batch_size, hidden_num, eta, nb_epochs])
-							print('batch value: \n', batch_size)
-							print('nb_epoch value: \n', nb_epochs)
-							print('hidden_run: ', hidden_num)
-							print('eta:', eta)
+					if n == nb_epochs-1:
+						models.append([model, loss_total/len(val_loader), batch_size, hidden_num, eta, nb_epochs])
+						# print('batch value: \n', batch_size)
+						# print('nb_epoch value: \n', nb_epochs)
+						# print('hidden_run: ', hidden_num)
+						# print('eta:', eta)
 
 bestModel = min(models, key=lambda x: x[1])		
 model = bestModel[0]
-print("Meilleur hyper-paramètre \n")
-print('batch value: \n', bestModel[2])
-print('nb_epoch value: \n', bestModel[3])
-print('hidden_run: ', bestModel[4])
-print('eta:', bestModel[5])
+print("Meilleur hyper-paramètre \n", bestModel)
 acc = 0.
 # on lit toutes les donnéees de test
 for x,t in test_loader:
