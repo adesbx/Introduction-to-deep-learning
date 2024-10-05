0f3b7cd2867dde61c2f91a1604c37e721668743a
Cuzin-Rambaud <valentincuzinmurena.io>
2024-10-05 18:54:57 +0200

    RunsOptuna, end of project

--- src/cnnOptuna.py
+++ src/cnnOptuna.py
@@ -148,7 +148,7 @@ def objective(trial: Trial) -> float:
 if __name__ == "__main__":
     core = core()
     study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=10)
+    study.optimize(objective, n_trials=1000,  timeout=10800)
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
